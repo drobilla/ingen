c1720ca9b8c92e3a4c22171d6fa3c0381501bc4e
David Robillard <d@drobilla.net>
2006-06-20 12:19:02 +0000

    Added missing patch port files
    git-svn-id: http://svn.drobilla.net/lad/grauph@67 a436a847-0d15-0410-975c-d299462d15a1

---
+++ src/progs/gtk/OmPatchPort.h
@@ -0,0 +1,60 @@
+/* This file is part of Om.  Copyright (C) 2006 Dave Robillard.
+ * 
+ * Om is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * Om is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef OMPATCHPORT_H
+#define OMPATCHPORT_H
+
+#include <cassert>
+#include <string>
+#include <flowcanvas/Port.h>
+#include "util/CountedPtr.h"
+
+namespace LibOmClient { class PortModel; }
+using namespace LibOmClient;
+using namespace LibFlowCanvas;
+using std::string; using std::list;
+
+namespace OmGtk {
+	
+class FlowCanvas;
+class PatchController;
+class PatchWindow;
+class OmPortModule;
+
+
+/** A Port (on a pseudo node) in a patch canvas, to represent a port on that patch.
+ * 
+ * \ingroup OmGtk
+ */
+class OmPatchPort : public LibFlowCanvas::Port
+{
+public:
+	OmPatchPort(OmPortModule* module, CountedPtr<PortModel> pm);
+
+	virtual ~OmPatchPort() {}
+
+	//void set_name(const string& n);
+	
+	CountedPtr<PortModel> model() const { return m_port_model; }
+	
+private:
+	CountedPtr<PortModel> m_port_model;
+};
+
+
+} // namespace OmGtk
+
+#endif // OMPATCHPORT_H
---
+++ src/progs/gtk/OmPortModule.h
@@ -0,0 +1,76 @@
+/* This file is part of Om.  Copyright (C) 2006 Dave Robillard.
+ * 
+ * Om is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * Om is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#ifndef OMPORTMODULE_H
+#define OMPORTMODULE_H
+
+#include <string>
+#include <libgnomecanvasmm.h>
+#include <flowcanvas/Module.h>
+#include "PortController.h"
+using std::string;
+
+namespace LibOmClient {
+class PortModel;
+class NodeModel;
+class ControlModel;
+}
+using namespace LibOmClient;
+
+namespace OmGtk {
+	
+class PatchController;
+class PortController;
+class OmFlowCanvas;
+class OmPort;
+
+
+/** A module in a patch.
+ *
+ * This base class is extended for various types of modules - SubpatchModule,
+ * DSSIModule, etc.
+ *
+ * \ingroup OmGtk
+ */
+class OmPortModule : public LibFlowCanvas::Module
+{
+public:
+	OmPortModule(OmFlowCanvas* canvas, PortController* port, double x, double y);
+	virtual ~OmPortModule() {}
+	
+	//virtual OmPort* port(const string& port_name) {
+	//	return (OmPort*)Module::port(port_name);
+	//}
+
+	virtual void store_location();
+	void move_to(double x, double y);
+
+	//void on_right_click(GdkEventButton* event) { m_port->show_menu(event); }
+	
+	PortController* port() const { return m_port; }
+
+protected:
+	//virtual void on_double_click(GdkEventButton* ev) { show_control_window(); }
+	//virtual void on_middle_click(GdkEventButton* ev) { show_control_window(); }
+	
+	PortController* m_port;
+};
+
+
+} // namespace OmGtk
+
+#endif // OMPORTMODULE_H
