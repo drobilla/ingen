05f87f09cc493cd58e67a299275cc98a724d9288
David Robillard <d@drobilla.net>
2007-07-07 17:11:40 +0000

    Added missing file.
    git-svn-id: http://svn.drobilla.net/lad/ingen@581 a436a847-0d15-0410-975c-d299462d15a1

---
+++ src/libs/client/PortModel.cpp
@@ -0,0 +1,50 @@
+/* This file is part of Ingen.
+ * Copyright (C) 2007 Dave Robillard <http://drobilla.net>
+ * 
+ * Ingen is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * Ingen is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "PortModel.h"
+
+namespace Ingen {
+namespace Client {
+
+
+bool
+PortModel::is_logarithmic() const
+{
+	const Atom& hint = get_metadata("ingen:logarithmic");
+	return (hint && hint > 0);
+}
+
+
+bool
+PortModel::is_integer() const
+{
+	const Atom& hint = get_metadata("ingen:integer");
+	return (hint && hint > 0);
+}
+
+
+bool
+PortModel::is_toggle() const
+{
+	const Atom& hint = get_metadata("ingen:toggled");
+	return (hint && hint > 0);
+}
+
+
+
+} // namespace Client
+} // namespace Ingen
