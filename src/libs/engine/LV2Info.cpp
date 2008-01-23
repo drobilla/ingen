a92596b71f1fed69e0c52f587dbba1da8b5a109c
David Robillard <d@drobilla.net>
2008-01-23 05:38:45 +0000

    Added missing file.
    git-svn-id: http://svn.drobilla.net/lad/ingen@1100 a436a847-0d15-0410-975c-d299462d15a1

---
+++ src/libs/engine/LV2Info.cpp
@@ -0,0 +1,83 @@
+/* This file is part of Ingen.
+ * Copyright (C) 2008 Dave Robillard <http://drobilla.net>
+ * 
+ * Ingen is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * Ingen is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#define __STDC_LIMIT_MACROS 1
+#include <cassert>
+#include <iostream>
+#include <stdint.h>
+#include "LV2Info.hpp"
+
+using namespace std;
+
+namespace Ingen {
+	
+LV2Info::LV2Info(SLV2World world)
+	: input_class(slv2_value_new_uri(world, SLV2_PORT_CLASS_INPUT))
+	, output_class(slv2_value_new_uri(world, SLV2_PORT_CLASS_OUTPUT))
+	, control_class(slv2_value_new_uri(world, SLV2_PORT_CLASS_CONTROL))
+	, audio_class(slv2_value_new_uri(world, SLV2_PORT_CLASS_AUDIO))
+	, event_class(slv2_value_new_uri(world, SLV2_PORT_CLASS_EVENT))
+	, next_uri_id(1)
+	, lv2_features(new LV2_Feature*[2])
+{
+	uri_map_feature_data.uri_to_id = &LV2Info::uri_map_uri_to_id;
+	uri_map_feature_data.callback_data = this;
+	uri_map_feature.URI = LV2_URI_MAP_URI;
+	uri_map_feature.data = &uri_map_feature_data;
+
+	lv2_features[0] = &uri_map_feature;
+	lv2_features[1] = NULL;
+}
+
+
+LV2Info::~LV2Info()
+{
+	slv2_value_free(input_class);
+	slv2_value_free(output_class);
+	slv2_value_free(control_class);
+	slv2_value_free(audio_class);
+	slv2_value_free(event_class);
+}
+
+
+	
+uint32_t
+LV2Info::uri_map_uri_to_id(LV2_URI_Map_Callback_Data callback_data,
+                           const char*               map,
+                           const char*               uri)
+{
+	// TODO: map ignored, < UINT16_MAX assumed
+	
+	LV2Info* me = (LV2Info*)callback_data;
+	uint32_t ret = 0;
+
+	URIMap::iterator i = me->uri_map.find(uri);
+	if (i != me->uri_map.end()) {
+		ret = i->second;
+	} else {
+		ret = me->next_uri_id++;
+		me->uri_map.insert(make_pair(string(uri), ret));
+	}
+	
+	cout << "URI MAP (" << map << "): " << uri << " -> " << ret << endl; 
+
+	assert(ret <= UINT16_MAX);
+	return ret;
+}
+
+
+} // namespace Ingen
