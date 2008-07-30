5b83be147cfa8ab90ff38f824e9a88d51c1a37de
David Robillard <d@drobilla.net>
2008-07-30 01:41:19 +0000

    Update Client interface used by bindings.
    git-svn-id: http://svn.drobilla.net/lad/ingen@1313 a436a847-0d15-0410-975c-d299462d15a1

--- src/bindings/Client.hpp
+++ src/bindings/Client.hpp
@@ -57,9 +57,15 @@ public:
 	                      const std::string& data_type,
 	                      bool               is_output)  {}
 	
+	virtual void polyphonic(const std::string& path,
+	                        bool               polyphonic) {}
+	
 	virtual void patch_enabled(const std::string& path)  {}
 	
 	virtual void patch_disabled(const std::string& path)  {}
+	
+	virtual void patch_polyphony(const std::string& path,
+	                             uint32_t           poly) = 0;
 	
 	virtual void patch_cleared(const std::string& path)  {}
 	
