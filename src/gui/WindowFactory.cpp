477b7fed8082e72b5d04bc83c6a6f856b8a8889f
David Robillard <d@drobilla.net>
2009-05-30 18:24:37 +0000

    Fix path in dynamic load plugin window title (don't show path: URI prefix).
    git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@2054 a436a847-0d15-0410-975c-d299462d15a1

--- src/gui/WindowFactory.cpp
+++ src/gui/WindowFactory.cpp
@@ -268,7 +268,7 @@ WindowFactory::present_load_plugin(SharedPtr<PatchModel> patch, GraphObject::Pro
 	int width, height;
 	w->second->get_size(width, height);
 	_load_plugin_win->set_default_size(width - width / 8, height / 2);
-	_load_plugin_win->set_title(string("Load Plugin - ") + patch->path().str() + " - Ingen");
+	_load_plugin_win->set_title(string("Load Plugin - ") + patch->path().chop_scheme() + " - Ingen");
 	_load_plugin_win->present(patch, data);
 }
 
