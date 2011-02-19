db6f76316b6ec7c9ab32f7efd5dce81a976cba82
David Robillard <d@drobilla.net>
2011-02-19 05:19:43 +0000

    Show human name instead of symbol in node control window (fix ticket #637).
    git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@2995 a436a847-0d15-0410-975c-d299462d15a1

--- src/gui/NodeControlWindow.cpp
+++ src/gui/NodeControlWindow.cpp
@@ -45,7 +45,7 @@ NodeControlWindow::NodeControlWindow(SharedPtr<NodeModel> node, uint32_t poly)
 	property_resizable() = true;
 	set_border_width(5);
 
-	set_title(_node->path().chop_scheme() + " Controls - Ingen");
+	set_title(_node->plugin_model()->human_name() + " - Ingen");
 
 	Glib::RefPtr<Gnome::Glade::Xml> xml = GladeFactory::new_glade_reference("warehouse_win");
 	xml->get_widget_derived("control_panel_vbox", _control_panel);
