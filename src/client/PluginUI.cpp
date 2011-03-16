drobilla/ingen

commit 91aa0bcb72b365392a9352d7e82e9dc6058b39a8

Fix crash when double clicking nodes without custom UIs. Fix LV2 UI memory errors.
git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@3093 a436a847-0d15-0410-975c-d299462d15a1

src/client/PluginUI.cpp
@@ -126,13 +126,19 @@ PluginUI::create(Ingen::Shared::World* world,
 		PluginUI::ui_host = slv2_ui_host_new(lv2_ui_write, NULL, NULL, NULL);
 	}
 
-	SharedPtr<PluginUI> ret(new PluginUI(world, node));
-	ret->_features = world->lv2_features()->lv2_features(world, node.get());
-
 	SLV2Value gtk_ui = slv2_value_new_uri(
 		world->slv2_world(), "http://lv2plug.in/ns/extensions/ui#GtkUI");
 
 	SLV2UI ui = slv2_plugin_get_default_ui(plugin, gtk_ui);
+
+	slv2_value_free(gtk_ui);
+
+	if (!ui) {
+		return SharedPtr<PluginUI>();
+	}
+
+	SharedPtr<PluginUI> ret(new PluginUI(world, node));
+	ret->_features = world->lv2_features()->lv2_features(world, node.get());
 
 	SLV2UIInstance instance = slv2_ui_instance_new(
 		plugin,
@@ -142,8 +148,6 @@ PluginUI::create(Ingen::Shared::World* world,
 		ret.get(),
 		ret->_features->array());
 
-	slv2_ui_host_free(ui_host);
-	
 	if (instance) {
 		ret->_instance = instance;
 	} else {

commit b3786217c82ff2f12a73595ecc375916d6e9d169

Fix LV2 plugin UIs (broken in r3093).
git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@3098 a436a847-0d15-0410-975c-d299462d15a1

src/client/PluginUI.cpp
@@ -131,9 +131,8 @@ PluginUI::create(Ingen::Shared::World* world,
 
 	SLV2UI ui = slv2_plugin_get_default_ui(plugin, gtk_ui);
 
-	slv2_value_free(gtk_ui);
-
 	if (!ui) {
+		slv2_value_free(gtk_ui);
 		return SharedPtr<PluginUI>();
 	}
 
@@ -147,6 +146,8 @@ PluginUI::create(Ingen::Shared::World* world,
 		ui_host,
 		ret.get(),
 		ret->_features->array());
+
+	slv2_value_free(gtk_ui);
 
 	if (instance) {
 		ret->_instance = instance;