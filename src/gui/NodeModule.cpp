e18380569bdbe1926be7540f3e2f9ebdf49a8e70
David Robillard <d@drobilla.net>
2011-09-06 18:01:38 +0000

    Fix crash when clicking patch modules (fix #769).
    git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@3459 a436a847-0d15-0410-975c-d299462d15a1

--- src/gui/NodeModule.cpp
+++ src/gui/NodeModule.cpp
@@ -416,7 +416,10 @@ NodeModule::set_selected(bool b)
 		if (b) {
 			PatchWindow* win = app.window_factory()->parent_patch_window(node());
 			if (win) {
-				const std::string& doc = node()->plugin_model()->documentation();
+				std::string doc;
+				if (node()->plugin_model()) {
+					doc = node()->plugin_model()->documentation();
+				}
 				if (!doc.empty()) {
 					win->doc_textview()->get_buffer()->set_text(doc);
 					win->doc_textview()->show();
