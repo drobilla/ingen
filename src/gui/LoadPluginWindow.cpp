f888ae54a7ad5f2ab274573651ee09c3fa6daa8a
David Robillard <d@drobilla.net>
2012-03-27 05:38:12 +0000

    Fix load plugin window.
    git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@4115 a436a847-0d15-0410-975c-d299462d15a1

--- src/gui/LoadPluginWindow.cpp
+++ src/gui/LoadPluginWindow.cpp
@@ -327,7 +327,7 @@ LoadPluginWindow::load_plugin(const Gtk::TreeModel::iterator& iter)
 	const URIs&                  uris       = _app->uris();
 	Gtk::TreeModel::Row          row        = *iter;
 	SharedPtr<const PluginModel> plugin     = row.get_value(_plugins_columns._col_plugin);
-	bool                         polyphonic = _polyphonic_checkbutton->get_active();
+	//bool                         polyphonic = _polyphonic_checkbutton->get_active();
 	string                       name       = _node_name_entry->get_text();
 
 	if (name.empty())
@@ -342,9 +342,13 @@ LoadPluginWindow::load_plugin(const Gtk::TreeModel::iterator& iter)
 	} else {
 		Path path = _patch->path().base() + Path::nameify(name);
 		Resource::Properties props = _initial_data;
-		props.insert(make_pair(uris.rdf_type,         uris.ingen_Node));
-		props.insert(make_pair(uris.rdf_instanceOf,   _app->forge().alloc_uri(plugin->uri().str())));
-		props.insert(make_pair(uris.ingen_polyphonic, _app->forge().make(polyphonic)));
+		props.insert(make_pair(uris.rdf_type,
+		                       uris.ingen_Node));
+		props.insert(make_pair(uris.rdf_instanceOf,
+		                       _app->forge().alloc_uri(plugin->uri().str())));
+		Raul::warn << "FIXME: polyphonic" << std::endl;
+		//props.insert(make_pair(uris.ingen_polyphonic,
+		//                       _app->forge().make(polyphonic)));
 		_app->engine()->put(path, props);
 
 		if (_selection->get_selected_rows().size() == 1) {
