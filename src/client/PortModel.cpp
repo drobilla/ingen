005c93954d0e694f2b1733a395ceacdeb05302fc
David Robillard <d@drobilla.net>
2012-04-26 17:02:31 +0000

    Don't save ingen:activity property, which is transient.
    git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@4276 a436a847-0d15-0410-975c-d299462d15a1

--- src/client/PortModel.cpp
+++ src/client/PortModel.cpp
@@ -24,10 +24,12 @@ namespace Client {
 void
 PortModel::on_property(const Raul::URI& uri, const Raul::Atom& value)
 {
-	if (uri == _uris.ingen_value) {
-		this->value(value);
-	} else if (uri == _uris.ingen_activity) {
+	if (uri == _uris.ingen_activity) {
 		signal_activity().emit(value);
+	} else {
+		if (uri == _uris.ingen_value) {
+			this->value(value);
+		}
 	}
 	ObjectModel::on_property(uri, value);
 }
