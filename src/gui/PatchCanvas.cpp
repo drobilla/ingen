35edd6670930041f8d655612a275b2d453fc5c56
David Robillard <d@drobilla.net>
2012-04-26 23:28:15 +0000

    Fix undefined reference.
    git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@4277 a436a847-0d15-0410-975c-d299462d15a1

--- src/gui/PatchCanvas.cpp
+++ src/gui/PatchCanvas.cpp
@@ -399,9 +399,9 @@ PatchCanvas::add_node(SharedPtr<const NodeModel> nm)
 		module = SubpatchModule::create(*this, pm, _human_names);
 	} else {
 		module = NodeModule::create(*this, nm, _human_names);
-		const PluginModel* plugm = dynamic_cast<const PluginModel*>(nm->plugin());
-		if (plugm && !plugm->icon_path().empty())
-			module->set_icon(_app.icon_from_path(plugm->icon_path(), 100).operator->());
+		//const PluginModel* plugm = dynamic_cast<const PluginModel*>(nm->plugin());
+		//if (plugm && !plugm->icon_path().empty())
+		//	module->set_icon(_app.icon_from_path(plugm->icon_path(), 100).operator->());
 	}
 
 	module->show();
