f6bddf045a0d94766961b037d9d295eb12ca84f5
David Robillard <d@drobilla.net>
2012-05-11 04:07:05 +0000

    Fix compilation with GCC 4.7.
    git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@4346 a436a847-0d15-0410-975c-d299462d15a1

--- src/server/BufferRef.hpp
+++ src/server/BufferRef.hpp
@@ -14,8 +14,8 @@
   along with Ingen.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef INGEN_ENGINE_BUFFERREF_HPP
-#define INGEN_ENGINE_BUFFERREF_HPP
+#ifndef INGEN_ENGINE_BUFFER_REF_HPP
+#define INGEN_ENGINE_BUFFER_REF_HPP
 
 #include <boost/intrusive_ptr.hpp>
 
@@ -26,14 +26,10 @@ class Buffer;
 
 typedef boost::intrusive_ptr<Buffer> BufferRef;
 
+inline void intrusive_ptr_add_ref(Buffer* b) { b->ref(); }
+inline void intrusive_ptr_release(Buffer* b) { b->deref(); }
+
 } // namespace Server
 } // namespace Ingen
 
-namespace boost {
-
-inline void intrusive_ptr_add_ref(Ingen::Server::Buffer* b) { b->ref(); }
-inline void intrusive_ptr_release(Ingen::Server::Buffer* b) { b->deref(); }
-
-}
-
-#endif // INGEN_ENGINE_BUFFERREF_HPP
+#endif // INGEN_ENGINE_BUFFER_REF_HPP
