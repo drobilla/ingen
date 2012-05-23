2ea7d01d1878563f863763fc88df23dcbca8536a
David Robillard <d@drobilla.net>
2012-05-23 03:33:45 +0000

    Don't save ingen:activity as a persistent property.
    git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@4446 a436a847-0d15-0410-975c-d299462d15a1

--- src/client/PortModel.cpp
+++ src/client/PortModel.cpp
@@ -25,6 +25,7 @@ PortModel::on_property(const Raul::URI& uri, const Raul::Atom& value)
 {
 	if (uri == _uris.ingen_activity) {
 		signal_activity().emit(value);
+		return;
 	} else {
 		if (uri == _uris.ingen_value) {
 			this->value(value);
