drobilla/ingen

commit 10e9a3a800a35916872abf9e354be4c554338e4e

Use type safe enumerations.
git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@4918 a436a847-0d15-0410-975c-d299462d15a1

src/server/events/Connect.cpp
@@ -56,34 +56,34 @@ Connect::pre_process()
 
 	Node* tail = _engine.store()->get(_tail_path);
 	if (!tail) {
-		return Event::pre_process_done(NOT_FOUND, _tail_path);
+		return Event::pre_process_done(Status::NOT_FOUND, _tail_path);
 	}
 
 	Node* head = _engine.store()->get(_head_path);
 	if (!head) {
-		return Event::pre_process_done(NOT_FOUND, _head_path);
+		return Event::pre_process_done(Status::NOT_FOUND, _head_path);
 	}
 
 	OutputPort* tail_output = dynamic_cast<OutputPort*>(tail);
 	_head                   = dynamic_cast<InputPort*>(head);
 	if (!tail_output || !_head) {
-		return Event::pre_process_done(BAD_REQUEST, _head_path);
+		return Event::pre_process_done(Status::BAD_REQUEST, _head_path);
 	}
 
 	BlockImpl* const tail_block = tail_output->parent_block();
 	BlockImpl* const head_block = _head->parent_block();
 	if (!tail_block || !head_block) {
-		return Event::pre_process_done(PARENT_NOT_FOUND, _head_path);
+		return Event::pre_process_done(Status::PARENT_NOT_FOUND, _head_path);
 	}
 
 	if (tail_block->parent() != head_block->parent()
 	    && tail_block != head_block->parent()
 	    && tail_block->parent() != head_block) {
-		return Event::pre_process_done(PARENT_DIFFERS, _head_path);
+		return Event::pre_process_done(Status::PARENT_DIFFERS, _head_path);
 	}
 
 	if (!ArcImpl::can_connect(tail_output, _head)) {
-		return Event::pre_process_done(TYPE_MISMATCH, _head_path);
+		return Event::pre_process_done(Status::TYPE_MISMATCH, _head_path);
 	}
 
 	if (tail_block->parent_graph() != head_block->parent_graph()) {
@@ -103,7 +103,7 @@ Connect::pre_process()
 	}
 
 	if (_graph->has_arc(tail_output, _head)) {
-		return Event::pre_process_done(EXISTS, _head_path);
+		return Event::pre_process_done(Status::EXISTS, _head_path);
 	}
 
 	_arc = SharedPtr<ArcImpl>(new ArcImpl(tail_output, _head));
@@ -136,13 +136,13 @@ Connect::pre_process()
 		_compiled_graph = _graph->compile();
 	}
 
-	return Event::pre_process_done(SUCCESS);
+	return Event::pre_process_done(Status::SUCCESS);
 }
 
 void
 Connect::execute(ProcessContext& context)
 {
-	if (!_status) {
+	if (_status != Status::SUCCESS) {
 		_head->add_arc(context, _arc.get());
 		_engine.maid()->dispose(_head->set_buffers(context, _buffers));
 		_head->connect_buffers();
@@ -154,7 +154,7 @@ void
 Connect::post_process()
 {
 	Broadcaster::Transfer t(*_engine.broadcaster());
-	if (!respond()) {
+	if (respond() == Status::SUCCESS) {
 		_engine.broadcaster()->connect(_tail_path, _head_path);
 	}
 }

src/server/events/Disconnect.cpp
@@ -117,17 +117,17 @@ Disconnect::pre_process()
 	if (_tail_path.parent().parent() != _head_path.parent().parent()
 	    && _tail_path.parent() != _head_path.parent().parent()
 	    && _tail_path.parent().parent() != _head_path.parent()) {
-		return Event::pre_process_done(PARENT_DIFFERS, _head_path);
+		return Event::pre_process_done(Status::PARENT_DIFFERS, _head_path);
 	}
 
 	PortImpl* tail = dynamic_cast<PortImpl*>(_engine.store()->get(_tail_path));
 	if (!tail) {
-		return Event::pre_process_done(PORT_NOT_FOUND, _tail_path);
+		return Event::pre_process_done(Status::PORT_NOT_FOUND, _tail_path);
 	}
 
 	PortImpl* head = dynamic_cast<PortImpl*>(_engine.store()->get(_head_path));
 	if (!head) {
-		return Event::pre_process_done(PORT_NOT_FOUND, _head_path);
+		return Event::pre_process_done(Status::PORT_NOT_FOUND, _head_path);
 	}
 
 	BlockImpl* const src_block = tail->parent_block();
@@ -150,13 +150,13 @@ Disconnect::pre_process()
 	}
 
 	if (!_graph) {
-		return Event::pre_process_done(INTERNAL_ERROR, _head_path);
+		return Event::pre_process_done(Status::INTERNAL_ERROR, _head_path);
 	} else if (!_graph->has_arc(tail, head)) {
-		return Event::pre_process_done(NOT_FOUND, _head_path);
+		return Event::pre_process_done(Status::NOT_FOUND, _head_path);
 	}
 
 	if (src_block == NULL || dst_block == NULL) {
-		return Event::pre_process_done(PARENT_NOT_FOUND, _head_path);
+		return Event::pre_process_done(Status::PARENT_NOT_FOUND, _head_path);
 	}
 
 	_impl = new Impl(_engine,
@@ -167,7 +167,7 @@ Disconnect::pre_process()
 	if (_graph->enabled())
 		_compiled_graph = _graph->compile();
 
-	return Event::pre_process_done(SUCCESS);
+	return Event::pre_process_done(Status::SUCCESS);
 }
 
 bool
@@ -198,9 +198,9 @@ Disconnect::Impl::execute(ProcessContext& context, bool set_dst_buffers)
 void
 Disconnect::execute(ProcessContext& context)
 {
-	if (_status == SUCCESS) {
+	if (_status == Status::SUCCESS) {
 		if (!_impl->execute(context, true)) {
-			_status = NOT_FOUND;
+			_status = Status::NOT_FOUND;
 			return;
 		}
 
@@ -212,7 +212,7 @@ void
 Disconnect::post_process()
 {
 	Broadcaster::Transfer t(*_engine.broadcaster());
-	if (!respond()) {
+	if (respond() == Status::SUCCESS) {
 		_engine.broadcaster()->disconnect(_tail_path, _head_path);
 	}
 

commit 4e3f4a2a0fdca75352265ba8490fbe9f397e264a

Fix connection bug introduced in r4918 (fix #875).
git-svn-id: http://svn.drobilla.net/lad/trunk/ingen@4936 a436a847-0d15-0410-975c-d299462d15a1

src/server/events/Connect.cpp
@@ -142,7 +142,7 @@ Connect::pre_process()
 void
 Connect::execute(ProcessContext& context)
 {
-	if (_status != Status::SUCCESS) {
+	if (_status == Status::SUCCESS) {
 		_head->add_arc(context, _arc.get());
 		_engine.maid()->dispose(_head->set_buffers(context, _buffers));
 		_head->connect_buffers();

src/server/events/Disconnect.cpp
@@ -199,12 +199,11 @@ void
 Disconnect::execute(ProcessContext& context)
 {
 	if (_status == Status::SUCCESS) {
-		if (!_impl->execute(context, true)) {
+		if (_impl->execute(context, true)) {
+			_graph->set_compiled_graph(_compiled_graph);
+		} else {
 			_status = Status::NOT_FOUND;
-			return;
 		}
-
-		_graph->set_compiled_graph(_compiled_graph);
 	}
 }
 