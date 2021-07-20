4888033a88e53ea91fe88003708f0282578295db
Samuel Loewen <samuellwn@gmail.com>
2021-07-19 21:29:35 -0400

    Place newly created modules directly under the cursor

--- src/gui/GraphCanvas.cpp
+++ src/gui/GraphCanvas.cpp
@@ -945,14 +945,27 @@ GraphCanvas::get_new_module_location(double& x, double& y)
 Properties
 GraphCanvas::get_initial_data(Resource::Graph ctx)
 {
-	Properties result;
-	const URIs& uris = _app.uris();
-	result.emplace(uris.ingen_canvasX,
-	               Property(_app.forge().make(static_cast<float>(_menu_x)),
-	                        ctx));
-	result.emplace(uris.ingen_canvasY,
-	               Property(_app.forge().make(static_cast<float>(_menu_y)),
-	                        ctx));
+	Properties  result;
+	const URIs& uris  = _app.uris();
+	auto        alloc = widget().get_allocation();
+
+	int origin_x = 0;
+	int origin_y = 0;
+	widget().get_parent_window()->get_origin(origin_x, origin_y);
+
+	int scroll_x = 0;
+	int scroll_y = 0;
+	get_scroll_offsets(scroll_x, scroll_y);
+
+	const int canvas_x = _menu_x - origin_x - alloc.get_x() + scroll_x;
+	const int canvas_y = _menu_y - origin_y - alloc.get_y() + scroll_y;
+	result.emplace(
+		uris.ingen_canvasX,
+		Property(_app.forge().make(static_cast<float>(canvas_x)), ctx));
+	result.emplace(
+		uris.ingen_canvasY,
+		Property(_app.forge().make(static_cast<float>(canvas_y)), ctx));
+
 	return result;
 }
 
